1d45429d2e4ff7d759c23daa529be04b6880cf19
Diana Marcela Garcia Lopez <diana_ma.garcia@uao.edu.co>
2022-11-02 11:02:18 -0500

    cambios estilos

--- src/components/views/scout/objScout.jsx
+++ src/components/views/scout/objScout.jsx
@@ -1,18 +1,43 @@
 import "../../../styles/styles.css"
 
 import { useEventoStore } from "../../../Hooks/useEventoStore";
-import { useParams } from "react-router-dom";
+
 import { useSelector } from "react-redux";
+import MoodBadIcon from '@mui/icons-material/MoodBad';
+import InsertEmoticon from '@mui/icons-material/InsertEmoticon';
+import { useRamasStore } from "../../../Hooks";
+import { Button } from "@mui/material";
+import { useEffect } from "react";
+
 
 export const ObjScoutInsc = ({ scout}) => {
-    const params = useParams();
+   
+    const {user} = useSelector(state=>state.auth);
+    const { ramaScout } = useSelector(state => state.rama);
+    const {startListarRamaID}= useRamasStore();
+    const {startInscribirEvento}=useEventoStore();
+    useEffect(() => {
+        startListarRamaID(user?.uid)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+        function inscripcion(e) {
+        e.preventDefault();
+        startInscribirEvento(user?.uid);
+       }
 
     
     // }
+    if(ramaScout==='Recurso Adulto'){
+        return (
+            <Button variant="outlined" color="primary" onClick={inscripcion}>Inscribirse</Button>
+        )
+    }else{
+
+    
     if( scout === true ){
         return (
-            <div>
-    
+            <div className="inscrito-div">
+                <InsertEmoticon style={{ 'fontSize': "60px", 'width': "100%", "color":"#C1121F"}}/>
                 <h3>Actualmente se encuentra inscrito al evento</h3>
             </div>
     
@@ -20,13 +45,14 @@ export const ObjScoutInsc = ({ scout}) => {
 
     }else{
         return (
-            <div>
-    
+            <div className="inscrito-div">
+                <MoodBadIcon style={{ 'fontSize': "60px", 'width': "100%", "color":"#C1121F"}}/>
                 <h3>Actualmente  NO se encuentra inscrito al evento, solicite la inscripcion a su acudiente</h3>
             </div>
     
         )
 
     }
+}
     
 }
