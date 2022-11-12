e55ca44223e914f795e3e021438922bb48aec0dd
Diana Marcela Garcia Lopez <diana_ma.garcia@uao.edu.co>
2022-11-12 11:50:28 -0500

    Update CentinelApi.js

--- src/Api/CentinelApi.js
+++ src/Api/CentinelApi.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const CentinelApi = axios.create({
-    //baseURL: 'http://192.99.247.71:9001/api/'
+    baseURL: 'http://192.99.247.71:3001/api/'
     //baseURL: 'https://scout-centinelas113-prueba1.herokuapp.com/api'
-    baseURL:'http://localhost:9001/api'
+    //baseURL:'http://localhost:3001/api'
     
 });
 
